9da6be282bc6eee01ef898e72b5b5c4b4e57a45c
Samson Njai <njaisamson@gmail.com>
2024-08-28 22:08:29 +0300

    Adds exclamation mark on all the sentences

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_elements = [num for num in num_list if num % 2 == 0]
+    return even_elements
+
+return_evens([0, 1, 3, 5, 7, 8, 9])
 
 def make_exclamation(sentence_list):
-    pass
+    sentence_list_with_exclamation = [sentence + "!" for sentence in sentence_list]
+    
+    return sentence_list_with_exclamation
+
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
